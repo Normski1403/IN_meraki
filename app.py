b0376ef53ec958ab829b961910a7f7a7cd9f60b0
Carl Norman <carl.norman89@gmail.com>
2023-03-23 20:30:30 +0000

    Retreived organisation ID

--- app.py
+++ app.py
@@ -2,12 +2,17 @@
 ''' Script to poll the VF IN network dashboard for data
 '''
 import meraki
+import json
 
 
 def main():
     dashboard = meraki.DashboardAPI()
     my_orgs = dashboard.organizations.getOrganizations()
-    print(my_orgs)
+    org_id = my_orgs[0]['id']
+    print(org_id)
+    print(json.dumps(my_orgs, indent=4))
+    print('================================')
+    
 
 
 if __name__ == '__main__':
